d69c699b9de1b4cbd6f48935f022f6f790246da7
= <=>
2023-01-26 17:08:29 +0700

    init

---
+++ food-community-project/src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <p>trilaf's Foodary © 2023</p>
+        </div>
+    );
+}
+export default Footer;
---
+++ food-community-project/src/recipe.js
@@ -0,0 +1,41 @@
+export const my_recipe = {
+    recipe_1: {
+        name: 'Chirashi Don',
+        preparation_time: '30 minutes',
+        servings: '2',
+        ingredients: [
+            '1/2 cup rice',
+            '1/2 cup water',
+            '2 eggs',
+            '300g of salmon, tuna and salmon roe',
+        ],
+        instructions: [
+            'Cook rice and water in a rice cooker',
+            'Fry eggs in a pan',
+            'Serve rice, eggs and fish on a plate',
+        ],
+
+    },
+    recipe_2: {
+        name: 'Grated Coconut Omelet',
+        preparation_time: '20 minutes',
+        servings: '3',
+        ingredients: [
+            '5 chicken eggs or duck eggs',
+            'Toasted grated coconut',
+            '5 red onions, sliced',
+            'Sliced red chilies',
+            '2 stalks of celery, sliced',
+            '2 scallions, sliced',
+            '1 teaspoon of salt',
+            '1 teaspoon of pepper',
+            '1 teaspoon of mushroom stock',
+            'Cooking oil'
+        ],
+        instructions: [
+            'Whisk the eggs, add the spices and toasted grated coconut.',
+            'Fry with a little oil.',
+            'Turn and lift, then serve.'
+        ],
+    },
+}
